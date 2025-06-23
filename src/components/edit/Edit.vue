be579167dc72ad99a99c043632ddc97327a48ab4
TwilightFox <3150399699@qq.com>
2025-06-23 11:29:18 +0800

    添加侧边栏按钮，支持状态切换功能；调整相关样式以适应新功能

--- src/components/edit/Edit.vue
+++ src/components/edit/Edit.vue
@@ -5,16 +5,26 @@
         <img src="/left.png" />
       </button>
     </div>
-    <div class="sidebar">123</div>
+    <div class="sidebar">
+      <button class="sap" :class="{active: isActive}" @click="toggleSap">
+        {{ sap }}
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+const sap = ref("Start");
+const isActive = ref(false);
 const router = useRouter();
 const backToHome = () => {
   router.push("/");
+};
+const toggleSap = () => {
+  sap.value = sap.value === "Start" ? "Pause" : "Start";
+  isActive.value = !isActive.value;
 };
 </script>
 
@@ -24,8 +34,10 @@ const backToHome = () => {
   min-height: 100vh;
   width: 100%;
   flex-direction: row;
+
   .workspace {
     flex: 1;
+
     .back-button {
       margin-left: 40px;
       margin-top: 40px;
@@ -45,6 +57,23 @@ const backToHome = () => {
   .sidebar {
     width: 246px;
     border-left: 1px solid #cfd6e0;
+
+    .sap {
+      width: 200px;
+      height: 40px;
+      border-radius: 20px;
+      background: rgba(220, 232, 243, 1);
+      font-weight: 700;
+      cursor: pointer;
+      transition: 0.2s;
+      &.active {
+        background: #ff0000;
+        color: white;
+      }
+    }
+    button:focus {
+      outline: none;
+    }
   }
 }
 </style>
